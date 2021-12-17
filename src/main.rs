a2e8beff65635332a866d04cd68ecbfc3b43c15e
Norrick <Norrick.McGee@gmail.com>
2021-12-16 22:10:09 -0600

    egui helloworld added to project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use eframe::{egui, epi};
+
+#[derive(Default)]
+struct SqlPartyApp {}
+
+impl epi::App for SqlPartyApp {
+    fn name(&self) -> &str {
+        "SQL-Party"
+    }
+
+    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Time to Party!");
+        });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = SqlPartyApp::default();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
 }
