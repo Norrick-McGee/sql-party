609667408a8c39026f6ec7923c9bde2a8099cc86
Norrick <Norrick.McGee@gmail.com>
2021-12-18 14:57:29 -0600

    code editor and execute button added

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,16 @@ impl epi::App for SqlPartyApp {
     }
 
     fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Time to Party!");
+        egui::CentralPanel::default().show(&ctx, |ui| {
+            ui.add(egui::Label::new("SQL Party!"));
+
+            let mut mystr = String::from("mystr");
+            let text_edit = egui::TextEdit::multiline(&mut mystr);
+            let code_editor = ui.add(egui::TextEdit::code_editor(text_edit));
+
+            // if code_editor.changed() {}
+            // if code_editor.lost_focus() && ui.input().key_pressed(egui::Key::Enter) { }
+            if ui.button("Exec SQL").clicked() { /* take some action here */ }
         });
     }
 }
